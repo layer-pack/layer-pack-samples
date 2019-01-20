dc161742e079a9cfe2eb414034bde6d0468dc96c
nathb <nathb2b@gmail.com>
2019-01-20 13:56:08 +0100

    ^add cli & wp 4

---
+++ simple-inherit/parentProject/App/containers/MeteoInfos.js
@@ -0,0 +1,45 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2019. Wise Wild Web
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *  @author : Nathanael Braun
+ *  @contact : caipilabs@gmail.com
+ */
+
+import React                   from "react";
+import moment                  from "moment";
+import {reScope, scopeToProps} from "rscopes";
+import {asRef, asStore}        from "rscopes/spells";
+
+
+export default ( { meteoData } ) =>
+	<div className={ "MeteoInfos" }>
+		<div className={ "location" }>
+			{ meteoData.name }&nbsp;
+			(&nbsp;
+			{
+				meteoData.weather[0] &&
+				meteoData.weather[0].description
+			}
+			&nbsp;)
+		</div>
+		<div className={ "picto" }>
+			{
+				meteoData.weather[0] &&
+				<img
+					src={ "http://openweathermap.org/img/w/" + meteoData.weather[0].icon + '.png' }></img>
+			}
+		</div>
+		<div className={ "infos" }>
+			<div className={ "dt" }>
+			</div>
+			<div className={ "dt" }>{ moment(meteoData.dt * 1000).format('MMMM Do YYYY, h:mm:ss a') }</div>
+			<div className={ "wind" }>{ meteoData.wind.speed }mh</div>
+		</div>
+	</div>;
