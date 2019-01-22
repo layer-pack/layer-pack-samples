e664671593eb816d074430dd503a1c7e33d83d1d
nathb <nathb2b@gmail.com>
2019-01-22 21:59:38 +0100

    + hot reload in rescope

--- clever-inherit/parentProject/App/AppScope.js
+++ clever-inherit/parentProject/App/AppScope.js
@@ -47,12 +47,12 @@ export default {
 		},
 		updatePostIt( postIt ) {
 			return {
-				items: this.nextState.items
-				           .map(
-					           it => (it._id === postIt._id)
-					                 ? postIt
-					                 : it
-				           )
+				//items: this.nextState.items
+				//           .map(
+				//	           it => (it._id === postIt._id)
+				//	                 ? postIt
+				//	                 : it
+				//           )
 			}
 		},
 		rmPostIt( postIt ) {
